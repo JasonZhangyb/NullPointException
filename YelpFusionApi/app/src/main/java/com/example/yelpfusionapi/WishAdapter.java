22768a089040f8f7761f0fb6977bf31c91157d3e
Young <erin.chen095@gmail.com>
2019-04-12 16:52:00 -0400

    push yelp fushion api

---
+++ YelpFusionApi/app/src/main/java/com/example/yelpfusionapi/WishAdapter.java
@@ -0,0 +1,58 @@
+package com.example.yelpfusionapi;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WishAdapter extends RecyclerView.Adapter<WishAdapter.MyViewHolder> {
+
+    Context context;
+    ArrayList<Restaurant> restaurants;
+
+    public WishAdapter(Context c, ArrayList<Restaurant> r){
+        context = c;
+        restaurants = r;
+    }
+
+    @NonNull
+    @Override
+    public WishAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(context).inflate(R.layout.restaurant_item,viewGroup, false);
+        return  new MyViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) { //retrieve data and show it in the view
+        holder.tvName.setText(restaurants.get(position).getName());
+        Picasso.get().load(restaurants.get(position).getImageUrl()).into(holder.imgRestaurant);
+
+    }
+
+    @Override
+    public int getItemCount() {   //get the number of restaurants in wishlist
+        return restaurants.size();
+    }
+
+    class MyViewHolder extends RecyclerView.ViewHolder{
+        public TextView tvName;
+        public ImageView imgRestaurant;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            tvName = itemView.findViewById(R.id.RestaurantName);
+            imgRestaurant = itemView.findViewById(R.id.imgRestaurant);
+        }
+    }
+
+}
---
+++ YelpFusionApi/app/src/main/java/com/example/yelpfusionapi/WishList.java
@@ -0,0 +1,61 @@
+package com.example.yelpfusionapi;
+
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
+
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class WishList extends AppCompatActivity {
+
+    DatabaseReference reference;
+    RecyclerView recyclerView;
+    ArrayList<Restaurant>list;
+    WishAdapter adapter;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.wish_list);
+        Firebase.setAndroidContext(this);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        list = new ArrayList<Restaurant>();
+
+
+        reference = FirebaseDatabase.getInstance().getReference().child("Restaurants");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot dataSnapshot1: dataSnapshot.getChildren())
+                {
+                    Restaurant r = dataSnapshot1.getValue(Restaurant.class);
+                    list.add(r);
+                }
+                adapter = new WishAdapter(WishList.this,list);
+                recyclerView.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(WishList.this,"Opss...Something is wrong",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+}
